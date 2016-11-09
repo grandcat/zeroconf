grandcat/zeroconf

commit c4e0f84f481b082033991a01d2fc1ddfb777313d

Add support for broadcasting multiple IPs (mostly cherry-picked from Jono Spiro)

client.go
@@ -168,13 +168,13 @@ func (c *client) mainloop(params *LookupParams) {
 				case *dns.A:
 					for k, e := range entries {
 						if e.HostName == rr.Hdr.Name && entries[k].AddrIPv4 == nil {
-							entries[k].AddrIPv4 = rr.A
+							entries[k].AddrIPv4 = append(entries[k].AddrIPv4, rr.A)
 						}
 					}
 				case *dns.AAAA:
 					for k, e := range entries {
 						if e.HostName == rr.Hdr.Name && entries[k].AddrIPv6 == nil {
-							entries[k].AddrIPv6 = rr.AAAA
+							entries[k].AddrIPv6 = append(entries[k].AddrIPv6, rr.AAAA)
 						}
 					}
 				}

server.go
@@ -50,27 +50,22 @@ func Register(instance, service, domain string, port int, text []string, ifaces 
 		entry.HostName = fmt.Sprintf("%s.%s.", trimDot(entry.HostName), trimDot(entry.Domain))
 	}
 
-	// GetLocalIP returns the non loopback local IP of the host
+	// Enumerate IPs for all interfaces. For IPv6, it only takes reachable addresses.
 	iaddrs, err := net.InterfaceAddrs()
 	if err != nil {
 		return nil, err
 	}
 
 	for _, address := range iaddrs {
-		// check the address type and if it is not a loopback the display it
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				entry.AddrIPv4 = ipnet.IP
+				entry.AddrIPv4 = append(entry.AddrIPv4, ipnet.IP)
 			} else if ipnet.IP.To16() != nil {
-				// Favor global instead of local IP scope
-				if entry.AddrIPv6 != nil {
-					if ipnet.IP.IsGlobalUnicast() {
-						entry.AddrIPv6 = ipnet.IP
-						log.Printf("Added global IPv6: %v\n", entry.AddrIPv6)
-					}
-
+				if ipnet.IP.IsGlobalUnicast() {
+					entry.AddrIPv6 = append(entry.AddrIPv6, ipnet.IP)
+					log.Printf("Added global IPv6: %v\n", ipnet.IP)
 				} else {
-					entry.AddrIPv6 = ipnet.IP
+					log.Printf("Skipped IPv6: %v\n", ipnet.IP)
 				}
 			}
 		}
@@ -94,7 +89,7 @@ func Register(instance, service, domain string, port int, text []string, ifaces 
 
 // Register a service proxy by given argument. This call will skip the hostname/IP lookup and
 // will use the provided values.
-func RegisterProxy(instance, service, domain string, port int, host, ip string, text []string, ifaces []net.Interface) (*Server, error) {
+func RegisterProxy(instance, service, domain string, port int, host string, ips []string, text []string, ifaces []net.Interface) (*Server, error) {
 	entry := NewServiceEntry(instance, service, domain)
 	entry.Port = port
 	entry.Text = text
@@ -120,15 +115,17 @@ func RegisterProxy(instance, service, domain string, port int, host, ip string, 
 		entry.HostName = fmt.Sprintf("%s.%s.", trimDot(entry.HostName), trimDot(entry.Domain))
 	}
 
-	ipAddr := net.ParseIP(ip)
-	if ipAddr == nil {
-		return nil, fmt.Errorf("Failed to parse given IP: %v", ip)
-	} else if ipv4 := ipAddr.To4(); ipv4 != nil {
-		entry.AddrIPv4 = ipAddr
-	} else if ipv6 := ipAddr.To16(); ipv6 != nil {
-		entry.AddrIPv6 = ipAddr
-	} else {
-		return nil, fmt.Errorf("The IP is neither IPv4 nor IPv6: %#v", ipAddr)
+	for _, ip := range ips {
+		ipAddr := net.ParseIP(ip)
+		if ipAddr == nil {
+			return nil, fmt.Errorf("Failed to parse given IP: %v", ip)
+		} else if ipv4 := ipAddr.To4(); ipv4 != nil {
+			entry.AddrIPv4 = append(entry.AddrIPv4, ipAddr)
+		} else if ipv6 := ipAddr.To16(); ipv6 != nil {
+			entry.AddrIPv6 = append(entry.AddrIPv6, ipAddr)
+		} else {
+			return nil, fmt.Errorf("The IP is neither IPv4 nor IPv6: %#v", ipAddr)
+		}
 	}
 
 	s, err := newServer(ifaces)
@@ -143,7 +140,7 @@ func RegisterProxy(instance, service, domain string, port int, host, ip string, 
 	return s, nil
 }
 
-// Server structure incapsulates both IPv4/IPv6 UDP connections
+// Server structure encapsulates both IPv4/IPv6 UDP connections
 type Server struct {
 	service        *ServiceEntry
 	ipv4conn       *net.UDPConn
@@ -347,7 +344,7 @@ func (s *Server) composeBrowsingAnswers(resp *dns.Msg, ttl uint32) {
 	}
 	resp.Extra = append(resp.Extra, srv, txt)
 
-	if s.service.AddrIPv4 != nil {
+	for _, ipv4 := range s.service.AddrIPv4 {
 		a := &dns.A{
 			Hdr: dns.RR_Header{
 				Name:   s.service.HostName,
@@ -355,11 +352,11 @@ func (s *Server) composeBrowsingAnswers(resp *dns.Msg, ttl uint32) {
 				Class:  dns.ClassINET,
 				Ttl:    ttl,
 			},
-			A: s.service.AddrIPv4,
+			A: ipv4,
 		}
 		resp.Extra = append(resp.Extra, a)
 	}
-	if s.service.AddrIPv6 != nil {
+	for _, ipv6 := range s.service.AddrIPv6 {
 		aaaa := &dns.AAAA{
 			Hdr: dns.RR_Header{
 				Name:   s.service.HostName,
@@ -367,7 +364,7 @@ func (s *Server) composeBrowsingAnswers(resp *dns.Msg, ttl uint32) {
 				Class:  dns.ClassINET,
 				Ttl:    ttl,
 			},
-			AAAA: s.service.AddrIPv6,
+			AAAA: ipv6,
 		}
 		resp.Extra = append(resp.Extra, aaaa)
 	}
@@ -421,7 +418,7 @@ func (s *Server) composeLookupAnswers(resp *dns.Msg, ttl uint32) {
 	}
 	resp.Answer = append(resp.Answer, srv, txt, ptr, dnssd)
 
-	if s.service.AddrIPv4 != nil {
+	for _, ipv4 := range s.service.AddrIPv4 {
 		a := &dns.A{
 			Hdr: dns.RR_Header{
 				Name:   s.service.HostName,
@@ -429,11 +426,11 @@ func (s *Server) composeLookupAnswers(resp *dns.Msg, ttl uint32) {
 				Class:  dns.ClassINET | cache_flush,
 				Ttl:    120,
 			},
-			A: s.service.AddrIPv4,
+			A: ipv4,
 		}
 		resp.Answer = append(resp.Answer, a)
 	}
-	if s.service.AddrIPv6 != nil {
+	for _, ipv6 := range s.service.AddrIPv6 {
 		aaaa := &dns.AAAA{
 			Hdr: dns.RR_Header{
 				Name:   s.service.HostName,
@@ -441,7 +438,7 @@ func (s *Server) composeLookupAnswers(resp *dns.Msg, ttl uint32) {
 				Class:  dns.ClassINET | cache_flush,
 				Ttl:    120,
 			},
-			AAAA: s.service.AddrIPv6,
+			AAAA: ipv6,
 		}
 		resp.Answer = append(resp.Answer, aaaa)
 	}

service.go
@@ -80,8 +80,8 @@ type ServiceEntry struct {
 	Port     int      `json:"port"`     // Service Port
 	Text     []string `json:"text"`     // Service info served as a TXT record
 	TTL      uint32   `json:"ttl"`      // TTL of the service record
-	AddrIPv4 net.IP   `json:"-"`        // Host machine IPv4 address
-	AddrIPv6 net.IP   `json:"-"`        // Host machine IPv6 address
+	AddrIPv4 []net.IP `json:"-"`        // Host machine IPv4 address
+	AddrIPv6 []net.IP `json:"-"`        // Host machine IPv6 address
 }
 
 // Constructs a ServiceEntry structure by given arguments
@@ -92,7 +92,7 @@ func NewServiceEntry(instance, service, domain string) *ServiceEntry {
 		0,
 		[]string{},
 		0,
-		nil,
-		nil,
+		[]net.IP{},
+		[]net.IP{},
 	}
 }

commit 7db72af0f4fc37c672fa0ff4684ef32293f1c9a7

fix(client): IPs get lost during browse. Regression introduced by c4e0f84 .

client.go
@@ -167,13 +167,13 @@ func (c *client) mainloop(params *LookupParams) {
 					entries[rr.Hdr.Name].TTL = rr.Hdr.Ttl
 				case *dns.A:
 					for k, e := range entries {
-						if e.HostName == rr.Hdr.Name && entries[k].AddrIPv4 == nil {
+						if e.HostName == rr.Hdr.Name {
 							entries[k].AddrIPv4 = append(entries[k].AddrIPv4, rr.A)
 						}
 					}
 				case *dns.AAAA:
 					for k, e := range entries {
-						if e.HostName == rr.Hdr.Name && entries[k].AddrIPv6 == nil {
+						if e.HostName == rr.Hdr.Name {
 							entries[k].AddrIPv6 = append(entries[k].AddrIPv6, rr.AAAA)
 						}
 					}
@@ -192,7 +192,8 @@ func (c *client) mainloop(params *LookupParams) {
 					continue
 				}
 				// Require at least one resolved IP address for ServiceEntry
-				if e.AddrIPv4 == nil && e.AddrIPv6 == nil {
+				// TODO: wait some more time as chances are high both will arrive.
+				if len(e.AddrIPv4) == 0 && len(e.AddrIPv6) == 0 {
 					continue
 				}
 				params.Entries <- e