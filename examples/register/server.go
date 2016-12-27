776e2df23e608326c89236fae417be88534a366a
Stefan Smarzly <smarzlys@gmail.com>
2016-12-27 19:00:15 +0100

    feat(example): no timeout if 0 is passed

--- examples/register/server.go
+++ examples/register/server.go
@@ -14,7 +14,7 @@ import (
 
 var (
 	name     = flag.String("name", "GoZeroconfGo", "The name for the service.")
-	service  = flag.String("service", "_flexsmc._tcp", "Set the service type of the new service.")
+	service  = flag.String("service", "_workstation._tcp", "Set the service type of the new service.")
 	domain   = flag.String("domain", "local.", "Set the network domain. Default should be fine.")
 	port     = flag.Int("port", 42424, "Set the port the service is listening to.")
 	waitTime = flag.Int("wait", 10, "Duration in [s] to publish service for.")
@@ -37,10 +37,16 @@ func main() {
 	// Clean exit.
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	// Timeout timer.
+	var tc <-chan time.Time
+	if *waitTime > 0 {
+		tc = time.After(time.Second * time.Duration(*waitTime))
+	}
+
 	select {
 	case <-sig:
 		// Exit by user
-	case <-time.After(time.Second * time.Duration(*waitTime)):
+	case <-tc:
 		// Exit by timeout
 	}
 
