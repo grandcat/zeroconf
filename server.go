1e5bcc28d9ae568059729c5b59a5db3e949cc382
Aman Gupta <aman@tmm1.net>
2017-01-24 19:35:56 -0800

    confirm to spec when sending mdns responses

--- server.go
+++ server.go
@@ -301,6 +301,7 @@ func (s *Server) handleQuery(query *dns.Msg, from net.Addr) error {
 	for _, q := range query.Question {
 		resp := dns.Msg{}
 		resp.SetReply(query)
+		resp.Question = nil // RFC6762 section 6 "responses MUST NOT contain any questions"
 		resp.Answer = []dns.RR{}
 		resp.Extra = []dns.RR{}
 		if err = s.handleQuestion(q, &resp); err != nil {
